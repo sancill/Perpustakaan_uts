5350e0dfc45f52522f469e817bc9a82cfa13edf8
Sancill0912 <sandicilla0912@gmail.com>
2025-11-07 18:44:08 +0700

    add: ubah-kategori.php

---
+++ pages/ubah-kategori.php
@@ -0,0 +1,87 @@
+ <?php
+//proteksi agar file tidak dapat diakses langsung
+if(!defined('MY_APP')) {
+    die('Akses langsung tidak diperbolehkan!');
+}
+
+$sql = "SELECT * FROM kategori ORDER BY id_kategori DESC";
+
+$result = mysqli_query($koneksi, $sql);
+if (!$result) {
+    die("Query gagal: " . mysqli_error($koneksi));
+}
+
+if (isset($_GET['id']) && !empty ($_GET['id'])) {
+    $id_kategori = $_GET['id'];
+    $sql = "SELECT * FROM kategori WHERE id_kategori = ?";
+    if ($stmt = $koneksi->prepare($sql)) {
+        $stmt->bind_param("i", $id_kategori);
+        if ($stmt->execute()) {
+            $result = $stmt->get_result();
+            if ($result->num_rows == 1) {
+                // Kategori ditemukan
+                $kategori = $result->fetch_assoc();
+            } else {
+                echo "Kategori tidak ditemukan";
+                exit();
+            }
+            $stmt->close();
+        }
+    } else {
+        header("Location: index.php?hal=daftar-kategori");
+        exit();
+    }
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $nama_kategori = $_POST['nama_kategori'];
+
+    $sql = "UPDATE kategori SET nama_kategori = ? WHERE id_kategori = ?";
+    if ($stmt = $koneksi->prepare($sql)) {
+        $stmt->bind_param("si", $nama_kategori, $id_kategori);
+        if ($stmt->execute()) {
+            $pesan = "Kategori berhasil ditambahkan.";
+            $result_kategori = mysqli_query($koneksi, "SELECT * FROM kategori WHERE id_kategori = $id_kategori");
+            $kategori = $result_kategori->fetch_assoc();
+        } else {
+            $pesan_error = "Gagal menambahkan kategori ";
+        }
+        $stmt->close();
+    }
+    $koneksi->close();
+}
+        
+?>
+
+ <div class="container-fluid px-4">
+     <h1 class="mt-4">Kategori</h1>
+     <ol class="breadcrumb mb-4">
+         <li class="breadcrumb-item active">Ubah Kategori</li>
+     </ol>
+     <?php if (!empty($pesan)): ?>
+     <div class="alert alert-success" role="alert">
+         <?php echo $pesan ?>
+     </div>
+     <?php endif; ?>
+
+     <?php if (!empty($pesan_error)): ?>
+     <div class="alert alert-danger" role="alert">
+         <?php echo $pesan_error ?>
+     </div>
+     <?php endif; ?>
+
+
+     <div class="card mb-4">
+         <div class="card-body">
+             <form method="post">
+                 <div class="mb-3">
+                     <label for="nama_kategori" class="form-label">Nama Kategori</label>
+                     <input type="text" class="form-control" id="nama_kategori" name="nama_kategori"
+                         value="<?php echo $kategori['nama_kategori'] ?>" required>
+                 </div>
+                 <button type="submit" class="btn btn-primary">Submit</button>
+                 <a href="index.php?hal=daftar-kategori" class="btn btn-danger">kembali</a>
+             </form>
+         </div>
+     </div>
+ </div>
